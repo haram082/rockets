0f2c7fe3b49ef73708a28010491cb53a30a2debc
Haram <haramkim082@gmail.com>
2019-08-16 14:26:52 -0700

    Added a new class called card

--- python.py
+++ python.py
@@ -1,3 +1,11 @@
-def add(x, y):
-    return x+x
+class Card(object):
+    cardtype='Staff'
+    def __init__(self, name, attack, defense):
+        self.name = name
+        self.attack= attack
+        self.defense= defense
+    def power(self, other_card):
+        return (self.attack-self.defense)/2
+
+
     
