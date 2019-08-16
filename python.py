cdfb52fee2888cbc4ca0643fa3426de85f56e641
Haram <haramkim082@gmail.com>
2019-08-16 14:35:52 -0700

    added player class'

--- python.py
+++ python.py
@@ -6,6 +6,10 @@ class Card(object):
         self.defense= defense
     def power(self, other_card):
         return (self.attack-self.defense)/2
-
-
+class Player(object):
+    def __init__(self, deck, name):
+        self.deck=deck
+        self.name=name
+    def play(self, card_index):
+        self.deck.remove(card_index)
     
